87b2b6c2f78acef9ab49ed4258fc2ea5d101cd41
nitesh <niteshtagadghar3@gmail.com>
2024-02-18 02:32:15 +0530

    video start from there where left

--- video_player/app/player/Playlist.js
+++ video_player/app/player/Playlist.js
@@ -1,6 +1,5 @@
 "use client";
 import React, { useContext, useEffect, useRef } from 'react'
-import Data from './data'
 import { VideoDataContext } from '../page';
 
 function Playlist() {
@@ -8,10 +7,20 @@ function Playlist() {
     const dragItem = useRef(0)
     const dragOverItem = useRef(0)
 
-    const { setCurrentVideoInfo, videoRef, playList, setPlayList } = useContext(VideoDataContext)
+    const { currentVideoInfo, setCurrentVideoInfo, videoRef, playList, setPlayList } = useContext(VideoDataContext)
 
     // Play the video which is clicked and add an index to the object 
     const updateTheVideo = (videoRequest, idx) => {
+
+        // Store the video duration played 
+        let tempVideoList = [...playList]
+        tempVideoList.filter((item) => {
+            if (item.id === currentVideoInfo.id) {
+                item.playedDuration = videoRef.current.currentTime
+            }
+        })
+
+        setPlayList(tempVideoList)
         let videoToPlay = videoRequest
         videoRequest.index = idx
         setCurrentVideoInfo(videoToPlay)
@@ -38,16 +47,16 @@ function Playlist() {
     return (
         <div>
             {playList.map((item, idx) => {
-                return <div key={item.sources} onClick={() => updateTheVideo(item, idx)} className='relative flex space-x-3  hover:cursor-pointer h-16  hover:height-24 pb-2 px-2'
+                return <div key={item.sources} onClick={() => updateTheVideo(item, idx)} className='relative flex space-x-3  w-full hover:cursor-pointer h-20  hover:height-24 pb-2 px-2'
                     draggable
                     onDragStart={() => dragItem.current = idx}
                     onDragEnter={() => dragOverItem.current = idx}
                     onDragEnd={handleSort}
                     onDragOver={(e) => e.preventDefault()}
                 >
-                    <img className=' ' src={item.thumb} />
+                    <img src={item.thumb} />
                     <div className='flex justify-between w-full'>
-                        <p>{item.title}</p>
+                        <p className='font-semibold text-gray-900'>{item.title}</p>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>
