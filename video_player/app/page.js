95533b79caff8e12b0a39149e3a39502646ebe4c
nitesh <niteshtagadghar3@gmail.com>
2024-02-17 18:33:41 +0530

    play next video automated

--- video_player/app/page.js
+++ video_player/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, createContext, useEffect } from 'react';
+import React, { useState, createContext, useEffect, useRef } from 'react';
 import Player from './player/VideoPlayer';
 import Playlist from './player/Playlist';
 import Data from './player/data'
@@ -8,13 +8,17 @@ export const VideoDataContext = createContext()
 
 export default function Home() {
 
+  const videoRef = useRef(null)
   const [currentVideoInfo, setCurrentVideoInfo] = useState(Data[0])
 
-  useEffect(() => {
-    setCurrentVideoInfo(currentVideoInfo)
-  }, [currentVideoInfo])
-
-  console.log(currentVideoInfo, 'current video in main page')
+  // Wait for 3s and play next video once the current video ends 
+  const autoPlayNext = (index) => {
+    if (index) {
+      setTimeout(() => {
+        setCurrentVideoInfo(Data[index + 1])
+      }, 3000);
+    }
+  }
 
   return (
     <main >
@@ -22,7 +26,7 @@ export default function Home() {
         <h1 className='font-bold text-center p-4 '>Welcome to your favourate video player </h1>
       </div>
       <div className='flex justify-between m-8'>
-        <VideoDataContext.Provider value={{ currentVideoInfo, setCurrentVideoInfo }}>
+        <VideoDataContext.Provider value={{ currentVideoInfo, setCurrentVideoInfo, videoRef, autoPlayNext }}>
           <Player />
           <Playlist />
         </VideoDataContext.Provider>
--- video_player/app/player/Playlist.js
+++ video_player/app/player/Playlist.js
@@ -5,22 +5,25 @@ import { VideoDataContext } from '../page';
 
 function Playlist() {
 
-    const { setCurrentVideoInfo } = useContext(VideoDataContext)
+    const { setCurrentVideoInfo, videoRef } = useContext(VideoDataContext)
 
-    const updateTheVideo = (videoRequest) => {
-        setCurrentVideoInfo(videoRequest)
+    // Play the video which is clicked and add an index to the object 
+    const updateTheVideo = (videoRequest, idx) => {
+        let videoToPlay = videoRequest
+        videoRequest.index = idx
+        setCurrentVideoInfo(videoToPlay)
     }
 
+    // Let the video tag load the new src
     useEffect(() => {
-        const video = document.getElementById('hls-video');
-        video.pause()
-        video.load();
+        videoRef.current.pause()
+        videoRef.current.load()
     }, [updateTheVideo])
 
     return (
         <div>
-            {Data.map((item) => {
-                return <div key={item.sources} onClick={() => updateTheVideo(item)} className='flex hover:cursor-pointer h-16  hover:height-24 p-2'>
+            {Data.map((item, idx) => {
+                return <div key={item.sources} onClick={() => updateTheVideo(item, idx)} className='flex hover:cursor-pointer h-16  hover:height-24 pb-2 px-2'>
                     <img className=' ' src={item.thumb} />
                     <p>{item.title}</p>
                 </div>
